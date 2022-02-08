de35eed8124e1e4caa66aa14174843cba5c99b16
Patrick McGuire <insidenothing@gmail.com>
2022-02-08 14:03:34 -0500

    Update explorer.php

--- explorer.php
+++ explorer.php
@@ -160,10 +160,10 @@
       <nav class="container pt-4 mt-lg-3" aria-label="breadcrumb">
         <ol class="breadcrumb mb-0">
           <li class="breadcrumb-item">
-            <a href="index.html"><i class="bx bx-home-alt fs-lg me-1"></i>Home</a>
+            <a href="index.php"><i class="bx bx-home-alt fs-lg me-1"></i>Home</a>
           </li>
           <li class="breadcrumb-item">
-            <a href="blog-list-with-sidebar.html">Explorer</a>
+            <a href="blocks.php">Explorer</a>
           </li>
           <li class="breadcrumb-item active" aria-current="page">Block</li>
         </ol> 
