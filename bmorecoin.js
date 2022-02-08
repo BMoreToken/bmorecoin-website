146b0e8f457b7ebdc10be39ea08f56b2f1f4158d
Patrick McGuire <insidenothing@gmail.com>
2022-02-08 16:19:47 -0500

    Update bmorecoin.js

--- bmorecoin.js
+++ bmorecoin.js
@@ -2,3 +2,6 @@ function download(){
   window.open("https://github.com/BMoreToken/bmorecoinwallet/releases/download/1.1.0/ccx-desktop-win64-v1.1.0.zip");
 }
 
+function getWallet(){
+  window.open("https://github.com/BMoreToken/bmorecoinwallet/releases/download/1.1.0/ccx-desktop-win64-v1.1.0.zip");
+}
