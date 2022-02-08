9bf05421ad03bab5a87a750b68b9a39c6c5bc8e7
Patrick McGuire <insidenothing@gmail.com>
2022-02-08 14:02:12 -0500

    Update and rename blog-list-no-sidebar.html to blocks.php

--- blocks.php
+++ blocks.php
@@ -2,12 +2,12 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Silicon | Blog List No Sidebar</title>
+    <title>BMoreCoin | Block List</title>
 
     <!-- SEO Meta Tags -->
-    <meta name="description" content="Silicon - Multipurpose Technology Bootstrap Template">
+    <meta name="description" content="BMoreCoin - Multipurpose Cryptocurrency">
     <meta name="keywords" content="bootstrap, business, creative agency, mobile app showcase, saas, fintech, finance, online courses, software, medical, conference landing, services, e-commerce, shopping cart, multipurpose, shop, ui kit, marketing, seo, landing, blog, portfolio, html5, css3, javascript, gallery, slider, touch, creative">
-    <meta name="author" content="Createx Studio">
+    <meta name="author" content="BMoreCoin">
 
     <!-- Viewport -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -151,151 +151,15 @@
 
 
       <!-- Navbar -->
-      <!-- Remove "fixed-top" class to make navigation bar scrollable with the page -->
-      <header class="header navbar navbar-expand-lg navbar-light bg-light shadow-sm shadow-dark-mode-none fixed-top">
-        <div class="container px-3">
-          <a href="index.html" class="navbar-brand pe-3">
-            <img src="assets/img/logo.svg" width="47" alt="Silicon">
-            Silicon
-          </a>
-          <div id="navbarNav" class="offcanvas offcanvas-end">
-            <div class="offcanvas-header border-bottom">
-              <h5 class="offcanvas-title">Menu</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item dropdown">
-                  <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Landings</a>
-                  <div class="dropdown-menu p-0">
-                    <div class="d-lg-flex">
-                      <div class="mega-dropdown-column bg-position-center bg-repeat-0 bg-size-cover rounded-3 rounded-end-0" style="background-image: url(assets/img/landings.jpg); margin: -1px;"></div>
-                      <div class="mega-dropdown-column pt-lg-3 pb-lg-4">
-                        <ul class="list-unstyled mb-0">
-                          <li><a href="index.html" class="dropdown-item">Template Intro Page</a></li>
-                          <li><a href="landing-mobile-app-showcase.html" class="dropdown-item">Mobile App Showcase</a></li>
-                          <li><a href="landing-saas-v1.html" class="dropdown-item">SaaS v.1</a></li>
-                          <li><a href="landing-saas-v2.html" class="dropdown-item">SaaS v.2</a></li>
-                          <li><a href="landing-financial.html" class="dropdown-item">Financial Consulting</a></li>
-                          <li><a href="landing-online-courses.html" class="dropdown-item">Online Courses</a></li>
-                          <li><a href="landing-medical.html" class="dropdown-item">Medical</a></li>
-                          <li><a href="landing-software-company.html" class="dropdown-item">IT (Software) Company</a></li>
-                        </ul>
-                      </div>
-                      <div class="mega-dropdown-column pt-lg-3 pb-lg-4">
-                        <ul class="list-unstyled mb-0">
-                          <li><a href="landing-conference.html" class="dropdown-item">Conference</a></li>
-                          <li><a href="landing-digital-agency.html" class="dropdown-item">Digital Agency</a></li>
-                          <li><a href="landing-blog.html" class="dropdown-item">Blog Homepage</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-                <li class="nav-item dropdown">
-                  <a href="#" class="nav-link dropdown-toggle active" data-bs-toggle="dropdown" aria-current="page">Pages</a>
-                  <div class="dropdown-menu">
-                    <div class="d-lg-flex pt-lg-3">
-                      <div class="mega-dropdown-column">
-                        <h6 class="px-3 mb-2">About</h6>
-                        <ul class="list-unstyled mb-3">
-                          <li><a href="about-v1.html" class="dropdown-item py-1">About v.1</a></li>
-                          <li><a href="about-v2.html" class="dropdown-item py-1">About v.2</a></li>
-                        </ul>
-                        <h6 class="px-3 mb-2">Portfolio</h6>
-                        <ul class="list-unstyled mb-3">
-                          <li><a href="portfolio-grid.html" class="dropdown-item py-1">Grid View</a></li>
-                          <li><a href="portfolio-list.html" class="dropdown-item py-1">List View</a></li>
-                          <li><a href="portfolio-slider.html" class="dropdown-item py-1">Slider View</a></li>
-                          <li><a href="portfolio-courses.html" class="dropdown-item py-1">Courses</a></li>
-                          <li><a href="portfolio-single-project.html" class="dropdown-item py-1">Single Project</a></li>
-                          <li><a href="portfolio-single-course.html" class="dropdown-item py-1">Single Course</a></li>
-                        </ul>
-                      </div>
-                      <div class="mega-dropdown-column">
-                        <h6 class="px-3 mb-2">Blog</h6>
-                        <ul class="list-unstyled mb-3">
-                          <li><a href="blog-list-with-sidebar.html" class="dropdown-item py-1">List View with Sidebar</a></li>
-                          <li><a href="blog-grid-with-sidebar.html" class="dropdown-item py-1">Grid View with Sidebar</a></li>
-                          <li><a href="blog-list-no-sidebar.html" class="dropdown-item py-1">List View no Sidebar</a></li>
-                          <li><a href="blog-grid-no-sidebar.html" class="dropdown-item py-1">Grid View no Sidebar</a></li>
-                          <li><a href="blog-simple-feed.html" class="dropdown-item py-1">Simple Feed</a></li>
-                          <li><a href="blog-single.html" class="dropdown-item py-1">Single Post</a></li>
-                          <li><a href="blog-podcast.html" class="dropdown-item py-1">Podcast</a></li>
-                        </ul>
-                        <h6 class="px-3 mb-2">Services</h6>
-                        <ul class="list-unstyled mb-3">
-                          <li><a href="services.html" class="dropdown-item py-1">Services Page</a></li>
-                          <li><a href="services-single.html" class="dropdown-item py-1">Service Details</a></li>
-                        </ul>
-                      </div>
-                      <div class="mega-dropdown-column">
-                        <h6 class="px-3 mb-2">Contacts</h6>
-                        <ul class="list-unstyled mb-3">
-                          <li><a href="contacts-v1.html" class="dropdown-item py-1">Contacts v.1</a></li>
-                          <li><a href="contacts-v2.html" class="dropdown-item py-1">Contacts v.2</a></li>
-                        </ul>
-                        <h6 class="px-3 mb-2">Specialty</h6>
-                        <ul class="list-unstyled">
-                          <li><a href="404-v1.html" class="dropdown-item py-1">404 Error v.1</a></li>
-                          <li><a href="404-v2.html" class="dropdown-item py-1">404 Error v.2</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-                <li class="nav-item dropdown">
-                  <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-current="page">Account</a>
-                  <ul class="dropdown-menu">
-                    <li><a href="account-details.html" class="dropdown-item">Account Details</a></li>
-                    <li><a href="account-security.html" class="dropdown-item">Security</a></li>
-                    <li><a href="account-notifications.html" class="dropdown-item">Notifications</a></li>
-                    <li><a href="account-messages.html" class="dropdown-item">Messages</a></li>
-                    <li><a href="account-saved-items.html" class="dropdown-item">Saved Items</a></li>
-                    <li><a href="account-collections.html" class="dropdown-item">My Collections</a></li>
-                    <li><a href="account-payment.html" class="dropdown-item">Payment Details</a></li>
-                    <li><a href="account-signin.html" class="dropdown-item">Sign In</a></li>
-                    <li><a href="account-signup.html" class="dropdown-item">Sign Up</a></li>
-                  </ul>
-                </li>
-                <li class="nav-item">
-                  <a href="components/typography.html" class="nav-link">UI Kit</a>
-                </li>
-                <li class="nav-item">
-                  <a href="docs/getting-started.html" class="nav-link">Docs</a>
-                </li>
-              </ul>
-            </div>
-            <div class="offcanvas-footer border-top">
-              <a href="#" class="btn btn-primary w-100">
-                <i class="bx bx-cart fs-4 lh-1 me-1"></i>
-                &nbsp;Buy now
-              </a>
-            </div>      
-          </div>
-          <div class="form-check form-switch mode-switch pe-lg-1 ms-auto me-4" data-bs-toggle="mode">
-            <input type="checkbox" class="form-check-input" id="theme-mode">
-            <label class="form-check-label d-none d-sm-block" for="theme-mode">Light</label>
-            <label class="form-check-label d-none d-sm-block" for="theme-mode">Dark</label>
-          </div>
-          <button type="button" class="navbar-toggler" data-bs-toggle="offcanvas" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <a href="#" class="btn btn-primary btn-sm fs-sm rounded d-none d-lg-inline-flex">
-            <i class="bx bx-cart fs-5 lh-1 me-1"></i>
-            &nbsp;Buy now
-          </a>
-        </div>
-      </header>
-
+     <?PHP include_once('menu.php'); ?>
 
       <!-- Breadcrumb -->
       <nav class="container mt-lg-4 pt-5" aria-label="breadcrumb">
         <ol class="breadcrumb mb-0 pt-5">
           <li class="breadcrumb-item">
-            <a href="index.html"><i class="bx bx-home-alt fs-lg me-1"></i>Home</a>
+            <a href="index.php"><i class="bx bx-home-alt fs-lg me-1"></i>Home</a>
           </li>
-          <li class="breadcrumb-item active" aria-current="page">Blog List no Sidebar</li>
+          <li class="breadcrumb-item active" aria-current="page">Block List</li>
         </ol>
       </nav>
 
@@ -306,17 +170,17 @@
         <!-- Page title + Layout switcher + Search form -->
         <div class="row align-items-end gy-3 mb-4 pb-lg-3 pb-1">
           <div class="col-lg-5 col-md-4">
-            <h1 class="mb-2 mb-md-0">Blog List</h1>
+            <h1 class="mb-2 mb-md-0">Block List</h1>
           </div>
           <div class="col-lg-7 col-md-8">
             <div class="row gy-2">
               <div class="col-lg-5 col-sm-6">
                 <div class="d-flex align-items-center">
                   <div class="d-flex align-items-center me-sm-4 me-3">
-                    <a href="blog-list-no-sidebar.html" class="nav-link me-2 p-0 active">
+                    <a href="blocks.php" class="nav-link me-2 p-0 active">
                       <i class="bx bx-list-ul fs-4"></i>
                     </a>
-                    <a href="blog-grid-no-sidebar.html" class="nav-link p-0">
+                    <a href="blocks.php" class="nav-link p-0">
                       <i class="bx bx-grid-alt fs-4"></i>
                     </a>
                   </div>
@@ -786,7 +650,7 @@
         </div>
         <p class="fs-xs text-center text-md-start pb-2 pb-lg-0 mb-0">
           <span class="text-light opacity-50">&copy; All rights reserved. Made by </span>
-          <a class="nav-link d-inline-block p-0" href="https://createx.studio/" target="_blank" rel="noopener">Createx Studio</a>
+          <a class="nav-link d-inline-block p-0" href="https://www.bmorecoin.com" target="_blank" rel="noopener">BMoreCoin</a>
         </p>
       </div>
     </footer>
