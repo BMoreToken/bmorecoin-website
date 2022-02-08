b3ce3dd4138291a16b3e9441dd570f61cc64607a
Patrick McGuire <insidenothing@gmail.com>
2022-02-08 13:41:10 -0500

    Update about.php

--- about.php
+++ about.php
@@ -2,10 +2,10 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>About BMoreCoin</title>
+    <title>BMoreCoin | About</title>
 
     <!-- SEO Meta Tags -->
-    <meta name="description" content="BMoreCoin - Multipurpose Cryptocurrenty">
+    <meta name="description" content="BMoreCoin - Multipurpose Cryptocurrency">
     <meta name="keywords" content="bootstrap, business, creative agency, mobile app showcase, saas, fintech, finance, online courses, software, medical, conference landing, services, e-commerce, shopping cart, multipurpose, shop, ui kit, marketing, seo, landing, blog, portfolio, html5, css3, javascript, gallery, slider, touch, creative">
     <meta name="author" content="BMoreCoin">
 
@@ -160,7 +160,7 @@
           <li class="breadcrumb-item">
             <a href="index.html"><i class="bx bx-home-alt fs-lg me-1"></i>Home</a>
           </li>
-          <li class="breadcrumb-item active" aria-current="page">About BMoreCoin</li>
+          <li class="breadcrumb-item active" aria-current="page">About</li>
         </ol>
       </nav>
 
@@ -170,7 +170,7 @@
         <div class="row pt-lg-4 pb-lg-3 pb-xl-5">
           <div class="col-md-6">
             <h4 class="text-primary pb-1 mb-2">about</h4>
-            <h1 class="display-1">Silicon Inc.</h1>
+            <h1 class="display-1">BMoreCoin - Multipurpose Cryptocurrency</h1>
           </div>
           <div class="col-md-6 col-xl-5">
             <p class="fs-xl mb-1 mb-md-3">We are a dedicated team of passionate product managers, full stack developers, UX/UI designers, QA engineers and marketing experts helping businesses of every size — from new startups to public companies — launch their projects using our software .</p>
