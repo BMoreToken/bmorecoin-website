3f527e2a341e2df0f090c5cfea1695a3d2a4fed3
Patrick McGuire <insidenothing@gmail.com>
2022-02-11 18:35:42 -0500

    Update content.php

--- content.php
+++ content.php
@@ -1,13 +1,17 @@
 <?PHP
+// SEO
+$SEO_description  = 'BMoreCoin - Multipurpose Cryptocurrency';
+$SEO_keywords     = 'bootstrap, business, creative agency, mobile app showcase, saas, fintech, finance, online courses, software, medical, conference landing, services, e-commerce, shopping cart, multipurpose, shop, ui kit, marketing, seo, landing, blog, portfolio, html5, css3, javascript, gallery, slider, touch, creative';
+$SEO_author       = 'BMoreCoin Team';
+
+
 
 // Common Text
-
 $lower_paragraph  = 'On June 7th 2021 at 8:16:24 PM UTC the TX for <a target="_Blank" 
 href="https://etherscan.io/tx/0xa2ee423223507166dfee2895e3341b48f3bd8ea4d657881ab1ed58b6ec22e955">BMoreToken</a> was confirmed and the 
 <a target="_Blank" href="https://etherscan.io/address/0xbdd4f273c2b0f0b84a51bd733aac617d91159376#code">Contract Source Code Verified</a> 
 on the Ethereum Blockchain. On July 19th Jul 2021 2:34:21 AM EST the First Block of the <a target="_Blank" 
 href="https://www.bmorecoin.com/blockchain_explorer.php?block=ebe509d6ccf39f9b25112ea94daedca66f46547fedd7228ef9a42ad166948ae2">BMoreCoin</a> Blockchain was mined.';
-
 $lower_social_menu = '
 <li class="nav-item"><a href="https://www.facebook.com/BMoreToken" class="nav-link d-inline-block px-0 pt-1 pb-2">Facebook</a></li>
 <li class="nav-item"><a href="https://www.linkedin.com/company/bmoretoken" class="nav-link d-inline-block px-0 pt-1 pb-2">LinkedIn</a></li>
@@ -17,20 +21,18 @@ $lower_social_menu = '
 
 
 // About Page
-
 $our_company = 'Enim, vulputate felis, pharetra, leo diam est morbi. Gravida porta nunc vitae tempor sit proin lobortis mauris nulla. Eget felis nec 
 ultrices quis viverra aliquam aliquam. Eget vestibulum, orci, pulvinar diam. Ultrices tortor cursus porta magna sit blandit dolor, nulla 
 a. Sit diam non donec et, in interdum.';
-
 $client1 = "Dolor, a eget elementum, integer nulla volutpat, nunc, sit. Quam iaculis varius mauris magna sem. Egestas sed sed suscipit dolor faucibus dui imperdiet at eget. Tincidunt imperdiet quis hendrerit aliquam feugiat neque cras sed. Dictum quam integer volutpat tellus, faucibus platea. Pulvinar turpis proin faucibus at mauris. Sagittis gravida vitae porta enim, nulla arcu fermentum massa. Tortor ullamcorper lacus. Pellentesque lectus adipiscing aenean volutpat tortor habitant.";
 $client1_name = 'Ralph Edwards';
 $client1_title = 'Head of Marketing';
-
 $client2 = "Mi semper risus ultricies orci pulvinar in at enim orci. Quis facilisis nunc pellentesque in ullamcorper sit. Lorem blandit arcu sapien, senectus libero, amet dapibus cursus quam. Eget pellentesque eu purus volutpat adipiscing malesuada. Purus nisi, tortor vel lacus. Donec diam molestie ultrices vitae eget pulvinar fames. Velit lacus mi purus velit justo, amet. Nascetur lobortis diam, duis orci.";
 $client2_name = 'Annette Black';
 $client2_title = 'Strategic Advisor';
-
 $client3 = "Ac at sed sit senectus massa. Massa ante amet ultrices magna porta tempor. Aliquet diam in et magna ultricies mi at. Lectus enim, vel enim egestas nam pellentesque et leo. Elit mi faucibus laoreet aliquam pellentesque sed aliquet integer massa. Orci leo tortor ornare id mattis auctor aliquam volutpat aliquet. Odio lectus viverra eu blandit nunc malesuada vitae eleifend pulvinar. In ac fermentum sit in orci.";
 $client3_name = 'Darrell Steward';
 $client3_title = 'Project Manager';
+
+
 ?>
