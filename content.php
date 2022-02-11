2e1d7a9dea7f16d8efc981451fae06f84ae1548a
Patrick McGuire <insidenothing@gmail.com>
2022-02-11 18:18:24 -0500

    Create content.php

---
+++ content.php
@@ -0,0 +1,9 @@
+<?PHP
+
+$lower_paragraph  = 'On June 7th 2021 at 8:16:24 PM UTC the TX for <a target="_Blank" 
+href="https://etherscan.io/tx/0xa2ee423223507166dfee2895e3341b48f3bd8ea4d657881ab1ed58b6ec22e955">BMoreToken</a> was confirmed and the 
+<a target="_Blank" href="https://etherscan.io/address/0xbdd4f273c2b0f0b84a51bd733aac617d91159376#code">Contract Source Code Verified</a> 
+on the Ethereum Blockchain. On July 19th Jul 2021 2:34:21 AM EST the First Block of the <a target="_Blank" 
+href="https://www.bmorecoin.com/blockchain_explorer.php?block=ebe509d6ccf39f9b25112ea94daedca66f46547fedd7228ef9a42ad166948ae2">BMoreCoin</a> Blockchain was mined.';
+
+?>
