1745c88f4d93ebd03473d56c97794bfb2ce7e410
Patrick McGuire <insidenothing@gmail.com>
2022-02-21 16:23:21 -0500

    Update explorer.php

--- explorer.php
+++ explorer.php
@@ -8,7 +8,8 @@
     <?PHP include_once('content.php'); ?>
     <?PHP include_once('analytics.php'); ?>
     
-
+    <!-- jQuery 3.6.0 -->
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script> 
 
     <!-- SEO Meta Tags -->
     <meta name="description" content="<?PHP echo $SEO_description;?>">
