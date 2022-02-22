3e178f952aa41803a034b8ed4e984b69ae16dabb
Patrick McGuire <insidenothing@gmail.com>
2022-02-22 15:24:07 -0500

    Create breadcrumb.php

---
+++ breadcrumb.php
@@ -0,0 +1,12 @@
+<!-- Breadcrumb -->
+      <nav class="container pt-4 mt-lg-3" aria-label="breadcrumb">
+        <ol class="breadcrumb mb-0">
+          <li class="breadcrumb-item">
+            <a href="index.php"><i class="bx bx-home-alt fs-lg me-1"></i>Home</a>
+          </li>
+          <li class="breadcrumb-item">
+            <a href="blocks.php">Explorer</a>
+          </li>
+          <li class="breadcrumb-item active" aria-current="page">Block</li>
+        </ol> 
+      </nav>
