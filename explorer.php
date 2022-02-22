4a83339dbea2d60c489f9af58cb37f5011cf3ea4
Patrick McGuire <insidenothing@gmail.com>
2022-02-22 15:23:46 -0500

    Update explorer.php

--- explorer.php
+++ explorer.php
@@ -163,19 +163,7 @@
       <?PHP include_once('navbar.php'); ?>
 
 
-      <!-- Breadcrumb -->
-      <nav class="container pt-4 mt-lg-3" aria-label="breadcrumb">
-        <ol class="breadcrumb mb-0">
-          <li class="breadcrumb-item">
-            <a href="index.php"><i class="bx bx-home-alt fs-lg me-1"></i>Home</a>
-          </li>
-          <li class="breadcrumb-item">
-            <a href="blocks.php">Explorer</a>
-          </li>
-          <li class="breadcrumb-item active" aria-current="page">Block</li>
-        </ol> 
-      </nav>
-
+      
       
       
          <script>
