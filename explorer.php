dba00349592f92abae543276addcdb45e02d5477
Patrick McGuire <insidenothing@gmail.com>
2022-02-22 15:16:47 -0500

    Update explorer.php

--- explorer.php
+++ explorer.php
@@ -227,7 +227,7 @@
             <img src="assets/img/avatar/39.jpg" class="rounded-circle" width="60" alt="Avatar">
             <div class="ps-3">
               <h6 class="mb-1">Author</h6>
-              <a href="#" class="fw-semibold stretched-link">BMoreToken Team</a>
+              <a href="#" class="fw-semibold stretched-link">BMoreCoin Team</a>
             </div>
           </div>
         </div>
@@ -253,72 +253,6 @@
             <h2 class="h4">Latest Block Data</h2>
             <p id="explorer_box" class="mb-4 pb-2"></p>
             
-            <!-- Video -->
-            <div class="gallery mb-4 pb-2">
-              <a href="https://www.youtube.com/watch?v=YmYWAvp402M" class="gallery-item video-item is-hovered rounded-3" data-sub-html='<h6 class="fs-sm text-light">Video inside blog post</h6>'>
-                <img src="assets/img/blog/single/video-cover.jpg" alt="Video preview">
-              </a>
-            </div>
-
-            <p class="mb-4 pb-2">Tempor donec aliquam est, a. Sit arcu tellus pharetra, bibendum hendrerit arcu, sed. Scelerisque dui enim libero sit ac sed lacus lectus. Quam in iaculis scelerisque feugiat nibh mi. Maecenas posuere lobortis praesent iaculis sagittis. Egestas vel at praesent ipsum pretium faucibus adipiscing gravida feugiat. Lacus, rhoncus velit at non nunc, quam urna, phasellus facilisis. Cursus elit posuere amet tortor at. Mi tristique congue vulputate eget in pellentesque tincidunt. In viverra est id nibh. Nunc tellus ultrices magna turpis. Viverra est morbi posuere dapibus nisi habitasse. Id tristique cursus vel metus.</p>
-
-            <!-- Quotation -->
-            <figure class="position-relative mb-4 ps-4">
-              <span class="position-absolute top-0 start-0 w-3 h-100 bg-primary"></span>
-              <blockquote class="blockquote fs-xl fw-medium text-dark ps-1 ps-sm-3">
-                <p>Sollicitudin eget massa, elementum, purus nec fermentum vitae, elementum. Tincidunt vulputate lorem cursus id. Dictum tincidunt mi ornare tristique. Id sit elit pulvinar eu. Tempus vel, mauris sed proin aliquet vulputate cras est. Ut ornare eget a viverra.</p>
-              </blockquote>
-              <figcaption class="d-flex align-items-center pt-3 ps-1 ps-sm-3">
-                <img src="assets/img/avatar/04.jpg" width="48" class="rounded-circle" alt="Jane Cooper">
-                <div class="ps-3">
-                  <h6 class="fw-semibold lh-base mb-0">Jane Cooper</h6>
-                  <span class="fs-sm text-muted">CEO of Ipsum Company</span>
-                </div>
-              </figcaption>
-            </figure>
-
-            <p class="mb-4 pb-2">Pellentesque laoreet neque ut dictumst fames scelerisque. In malesuada orci dapibus risus tellus, amet, ultrices sagittis a. Turpis vel tincidunt tristique maecenas cursus tortor, suscipit ut. Ullamcorper at faucibus dolor tortor suspendisse suscipit senectus convallis aliquam. At orci amet facilisi nunc mi vitae iaculis diam a. Mauris venenatis sed purus cursus torte.</p>
-            <h2 class="h4">Paragraph Title 2</h2>
-            <p class="mb-4 pb-2">Praesent sed pulvinar posuere nisl tincidunt. Iaculis sit quam magna congue. Amet vel non aliquet habitasse. Egestas erat odio et, eleifend turpis etiam blandit interdum. Nec augue ut senectus quisque diam quis. At augue accumsan, in bibendum. A eget et, eget quisque egestas netus vel. Velit, aliquet turpis convallis ullamcorper. Scelerisque sagittis condimentum pretium in vitae etiam lacinia quis amet. Porttitor consequat, sollicitudin vivamus pharetra nibh faucibus neque, viverra. Praesent amet sed lacus vitae.</p>
-            <h4 class="h6">
-              <i class="bx bx-plus-circle me-1 mt-n1 align-middle fs-5 text-primary"></i>
-              PROS
-            </h4>
-            <ul class="mb-4 pb-2 ps-4">
-              <li class="mb-1">A eu, ac nunc, volutpat. Augue enim ac justo, at elementum, arcu.</li>
-              <li class="mb-1">At sodales quam felis ullamcorper iaculis tristique. Felis, etiam felis pellentesque sit neque.</li>
-              <li class="mb-1">Porta ipsum quis lacus eu ipsum mattis sit quis. Massa, massa lectus porttitor laoreet ultricies odio fermentum arcu quam.</li>
-              <li class="mb-1">Accumsan arcu neque, nisl, pellentesque fames justo consequat blandit lacus. Eget odio vel nulla vel.</li>
-              <li class="mb-1">Diam ac phasellus est, eu urna purus blandit aliquam. Vitae accumsan et pellentesque diam in.</li>
-              <li>Tellus arcu, lectus tincidunt neque nunc. Bibendum lacus, molestie ultrices sed arcu.</li>
-            </ul>
-            <h4 class="h6">
-              <i class="bx bx-minus-circle me-1 mt-n1 align-middle fs-5 text-primary"></i>
-              CONS
-            </h4>
-            <ul class="mb-4 pb-2 ps-4">
-              <li class="mb-1">Donec maecenas justo, et tortor quam elementum pharetra velit. Auctor dictum purus sollicitudin et quam vehicula amet lacus, integer.</li>
-              <li class="mb-1">Mi elit nibh erat facilisis. Volutpat eget malesuada mi in.</li>
-              <li class="mb-1">Tincidunt iaculis eleifend arcu egestas. Sit gravida vestibulum quam scelerisque.</li>
-              <li class="mb-1">Ornare elit, vel, ullamcorper nunc nulla pellentesque ut varius. Vitae tortor nulla a turpis erat fermentum, rhoncus.</li>
-              <li>Gravida cursus nunc habitant aliquet lacus. Tempus, interdum nullam non quam ipsum ultricies ac.</li>
-            </ul>
-            <h2 class="h4">Conclusion</h2>
-            <p class="mb-4 pb-2">Venenatis faucibus platea gravida amet sed sed urna volutpat. Aliquam neque, mi, justo odio semper. Egestas potentiquis neque nunc, rhoncus hendrerit. Viverra gravida pretium dolor eget placerat morbi proin eget. Vestibulum vitae ultrices vel sed sit quis. Ac quis diam at nulla libero. Turpis duis magna tellus condimentum.</p>
-            <p class="mb-4 pb-2">Velit parturient tellus tellus, congue pulvinar tellus viverra. In cum massa mattis ac. Amet vitae massa ut mi etiam. Auctor aliquam tristique felis donec eu sit nisi. Accumsan mauris eget convallis mattis sed etiam scelerisque.</p>
-
-            <!-- Tags -->
-            <hr class="mb-4">
-            <div class="d-flex flex-sm-row flex-column pt-2">
-              <h6 class="mt-sm-1 mb-sm-2 mb-3 me-2 text-nowrap">Related Tags:</h6>
-              <div>
-                <a href="#" class="btn btn-sm btn-outline-secondary me-2 mb-2">#bmorecoin</a>
-                <a href="#" class="btn btn-sm btn-outline-secondary me-2 mb-2">#block</a>
-                <a href="#" class="btn btn-sm btn-outline-secondary me-2 mb-2">#explorer</a>
-              </div>
-            </div>
-          </div>
-
           <!-- Sharing -->
           <div class="col-lg-3 position-relative">
             <div class="sticky-top ms-xl-5 ms-lg-4 ps-xxl-4" style="top: 105px !important;">
