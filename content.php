9e3b9272040af55d779fb52fcf9e135b91a48335
nobodyV2 <102918138+nobodyV2@users.noreply.github.com>
2022-04-03 13:41:03 -0400

    Update content.php

--- content.php
+++ content.php
@@ -27,7 +27,7 @@ $software_integration = 'Hello World';
 $mobile_app_development = 'Nunc, justo, diam orci, dictum purus convallis risus. Suscipit hendrerit at egestas id id blandit interdum est. Integer fames placerat turpis pretium quis hac leo lacus. Orci, dictum nunc mus quis semper eu bibendum enim, morbi.';
 $business_analytics = 'Gravida eget euismod tempus diam dignissim quam. Dignissim magnis blandit faucibus convallis augue nisl, etiam. Feugiat ut molestie non arcu senectus sed. Diam pellentesque sit mattis nec amet varius nunc a sed.';
 $software_testing = 'We make sure your software is working properly and runs smoothly.';
-$project_management = 'Massa dis morbi sagittis, tellus sit gravida. Id ut non ut in faucibus eu, ac. Tempus feugiat enim id pellentesque a sagittis vitae, convallis. Nunc, arcu enim orci ullamcorper aenean. Scelerisque eget a nibh bibendum commodo.';
+$project_management = 'hello world';
 
 // About Page
 $our_company = 'Enim, vulputate felis, pharetra, leo diam est morbi. Gravida porta nunc vitae tempor sit proin lobortis mauris nulla. Eget felis nec 
