5a7653d86bf966ba993868c6c3274b928c27994d
Patrick McGuire <insidenothing@gmail.com>
2022-04-04 00:14:50 -0400

    Update twitter.php

--- twitter.php
+++ twitter.php
@@ -21,18 +21,19 @@ echo "</pre><hr>";
 function getPage($url){
     $curl = curl_init();
     curl_setopt ($curl, CURLOPT_URL, $url);
-    curl_setopt ($curl, CURLOPT_USERAGENT, sprintf("McGuire Auto Data Entry/%d.0",rand(4,50)));
+    curl_setopt ($curl, CURLOPT_USERAGENT, sprintf("BMorCoin/%d.0",rand(40,500)));
     curl_setopt ($curl, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt ($curl, CURLOPT_SSL_VERIFYPEER, 0);
     curl_setopt ($curl, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . getenv('bearer_token')));
-    $html = curl_exec ($curl);
+    $json = curl_exec ($curl);
     curl_close ($curl);
-    return $html;
+    $array = json_decode($json, true);
+    return $array;
 }
 
-$tweets_json = getPage("https://api.twitter.com/2/tweets/search/recent?query=This+is+a+test+of+the+faucet+system");
+$tweets = getPage("https://api.twitter.com/2/tweets/search/recent?query=This+is+a+test+of+the+faucet+system");
 
-$tweets = json_decode($tweets_json, true);
+
     
 echo "<pre>";
 print_r($tweets);
