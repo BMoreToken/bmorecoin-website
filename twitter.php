d37ae0d99c618d9ad313dfd667a6f39bb7d4f1d6
Patrick McGuire <insidenothing@gmail.com>
2022-04-03 23:50:10 -0400

    Update twitter.php

--- twitter.php
+++ twitter.php
@@ -17,18 +17,25 @@ echo "<pre>";
 print_r($followers);
 echo "</pre><hr>";
 
+// Custom Code
+function getPage($url){
+    $curl = curl_init();
+    curl_setopt ($curl, CURLOPT_URL, $url);
+    curl_setopt ($curl, CURLOPT_USERAGENT, sprintf("McGuire Auto Data Entry/%d.0",rand(4,50)));
+    curl_setopt ($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt ($curl, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt ($curl, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . getenv('bearer_token')));
+    $html = curl_exec ($curl);
+    curl_close ($curl);
+    return $html;
+}
 
-$twitter = new BirdElephant($credentials);
+$tweets = getPage("https://api.twitter.com/2/tweets/search/recent?query=This+is+a+test+of+the+faucet+system");
 
-$tweets = $twitter->tweets();
+echo $tweets;
 
-$params = [
-    'query' => 'This is a test of the faucet system'
-];
-$tweets->search->recent($params);
-
-echo "<pre>";
-print_r($tweets);
-echo "</pre><hr>";
+//echo "<pre>";
+//print_r($tweets);
+//echo "</pre><hr>";
 
 ?>
