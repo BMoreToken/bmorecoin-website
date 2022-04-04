1ddbaca1939e3b26a7ae8eddcbf8bcd98cb53a02
Patrick McGuire <insidenothing@gmail.com>
2022-04-04 10:27:15 -0400

    fix footer

--- twitter.php
+++ twitter.php
@@ -391,35 +391,7 @@ echo "</pre><hr>";
 
 ?>
         
-      <!-- Contact form -->
-      <section class="container pb-3 mb-lg-3">
-        <div class="position-relative bg-secondary rounded-3 py-5 mb-2">
-          <div class="row pb-2 py-md-3 py-lg-5 px-4 px-lg-0 position-relative zindex-3">
-            <div class="col-xl-3 col-lg-4 col-md-5 offset-lg-1">
-              <p class="lead mb-2 mb-md-3">Ready to get started?</p>
-              <h2 class="h1 pb-3">Don’t Hesitate to Contact Us</h2>
-            </div>
-            <form class="col-lg-6 col-md-7 offset-xl-1 needs-validation" novalidate>
-              <div class="row">
-                <div class="col-sm-6 mb-4">
-                  <label for="name" class="form-label fs-base">Full name</label>
-                  <input type="text" id="name" class="form-control form-control-lg" required>
-                  <div class="invalid-feedback">Please enter your name!</div>
-                </div>
-                <div class="col-sm-6 mb-4">
-                  <label for="email" class="form-label fs-base">Email</label>
-                  <input type="email" id="email" class="form-control form-control-lg" required>
-                  <div class="invalid-feedback">Please provide a valid email address!</div>
-                </div>
-                <div class="col-12 pb-2 mb-4">
-                  <label for="message" class="form-label fs-base">Message</label>
-                  <textarea id="message" class="form-control form-control-lg" rows="4" required></textarea>
-                  <div class="invalid-feedback">Please enter your message!</div>
-                </div>
-              </div>
-              <button type="submit" class="btn btn-primary shadow-primary btn-lg">Send request</button>
-            </form>
-          </div>
+      <?PHP include_once('subscribe_large.php');?>
 
           <!-- Pattern -->
           <div class="position-absolute end-0 bottom-0 text-primary">
@@ -431,71 +403,7 @@ echo "</pre><hr>";
 
 
     <!-- Footer -->
-    <footer class="footer pt-5 pb-4 pb-lg-5">
-      <div class="container pt-lg-4">
-        <div class="row pb-5">
-          <div class="col-lg-4 col-md-6">
-            <div class="navbar-brand text-dark p-0 me-0 mb-3 mb-lg-4">
-              <img src="assets/img/logo.svg" width="47" alt="Silicon">
-              Silicon
-            </div>
-            <p class="fs-sm pb-lg-3 mb-4">Proin ipsum pharetra, senectus eget scelerisque varius pretium platea velit. Lacus, eget eu vitae nullam proin turpis etiam mi sit. Non feugiat feugiat egestas nulla nec. Arcu tempus, eget elementum dolor ullamcorper sodales ultrices eros.</p>
-            <form class="needs-validation" novalidate>
-              <label for="subscr-email" class="form-label">Subscribe to our newsletter</label>
-              <div class="input-group">
-                <input type="email" id="subscr-email" class="form-control rounded-start ps-5" placeholder="Your email" required>
-                <i class="bx bx-envelope fs-lg text-muted position-absolute top-50 start-0 translate-middle-y ms-3 zindex-5"></i>
-                <div class="invalid-tooltip position-absolute top-100 start-0">Please provide a valid email address.</div>
-                <button type="submit" class="btn btn-primary">Subscribe</button>
-              </div>
-            </form>
-          </div>
-          <div class="col-xl-6 col-lg-7 col-md-5 offset-xl-2 offset-md-1 pt-4 pt-md-1 pt-lg-0">
-            <div id="footer-links" class="row">
-              <div class="col-lg-4">
-                <h6 class="mb-2">
-                  <a href="#useful-links" class="d-block text-dark dropdown-toggle d-lg-none py-2" data-bs-toggle="collapse">Useful Links</a>
-                </h6>
-                <div id="useful-links" class="collapse d-lg-block" data-bs-parent="#footer-links">
-                  <ul class="nav flex-column pb-lg-1 mb-lg-3">
-                    <li class="nav-item"><a href="#" class="nav-link d-inline-block px-0 pt-1 pb-2">Home</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link d-inline-block px-0 pt-1 pb-2">About</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link d-inline-block px-0 pt-1 pb-2">Services</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link d-inline-block px-0 pt-1 pb-2">Works</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link d-inline-block px-0 pt-1 pb-2">Blog</a></li>
-                  </ul>
-                  <ul class="nav flex-column mb-2 mb-lg-0">
-                    <li class="nav-item"><a href="#" class="nav-link d-inline-block px-0 pt-1 pb-2">Terms &amp; Conditions</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link d-inline-block px-0 pt-1 pb-2">Privacy Policy</a></li>
-                  </ul>
-                </div>
-              </div>
-              <div class="col-xl-4 col-lg-3">
-                <h6 class="mb-2">
-                  <a href="#social-links" class="d-block text-dark dropdown-toggle d-lg-none py-2" data-bs-toggle="collapse">Socials</a>
-                </h6>
-                <div id="social-links" class="collapse d-lg-block" data-bs-parent="#footer-links">
-                  <ul class="nav flex-column mb-2 mb-lg-0">
-                    <li class="nav-item"><a href="#" class="nav-link d-inline-block px-0 pt-1 pb-2">Facebook</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link d-inline-block px-0 pt-1 pb-2">LinkedIn</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link d-inline-block px-0 pt-1 pb-2">Twitter</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link d-inline-block px-0 pt-1 pb-2">Instagram</a></li>
-                  </ul>
-                </div>
-              </div>
-              <div class="col-xl-4 col-lg-5 pt-2 pt-lg-0">
-                <h6 class="mb-2">Contact Us</h6>
-                <a href="mailto:email@example.com" class="fw-medium">email@example.com</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <p class="fs-xs text-center text-md-start pb-2 pb-lg-0 mb-0">
-          &copy; All rights reserved. Made by 
-          <a class="nav-link d-inline-block p-0" href="https://createx.studio/" target="_blank" rel="noopener">Createx Studio</a>
-        </p>
-      </div>
-    </footer>
+    <?PHP include_once('footer.php');?>
 
 
     <!-- Back to top button -->
