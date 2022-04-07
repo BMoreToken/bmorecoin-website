8f8ec44aea20b8639098e0a1e88800639cdbf0d8
Patrick McGuire <insidenothing@gmail.com>
2022-04-07 03:17:45 -0400

    Update wallet.php

--- wallet.php
+++ wallet.php
@@ -10,6 +10,24 @@ $date_format = 'F j, Y, g:i a T';
     <!-- Still Missing og:url, og:type, og:title, og:description, fb:app_id -->
     <title>BMoreCoin | Web Wallet</title>
     <!-- Custom Scripts -->
+    
+    <script>window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));</script>
+    
     <script type="text/javascript" src="bmorecoin.js"></script>
     <script type="text/javascript" src="https://www.google-analytics.com/analytics.js"></script>
     
